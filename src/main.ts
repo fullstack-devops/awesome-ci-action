8c23ee73616c6e9d4c2148a99478c2938ea6ba4e
eksrha <58111764+eksrha@users.noreply.github.com>
2022-01-17 11:08:51 +0100

    fix @actions/http-client

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
 import * as tc from '@actions/tool-cache';
 import * as core from '@actions/core';
 import * as path from 'path';
-import * as https from 'https';
 import * as fs from 'fs';
 
 async function run() {
     const wantedVersion = core.getInput('version');
     const wantedArch = core.getInput('arch');
-
-    const actionsPath = process.env.GITHUB_ACTION_PATH
-    const awesomeCiName = actionsPath + "/awesome-ci"
+    // const awesomeCiName = actionsPath + "/awesome-ci"
 
     // Download awesome-ci
     const repo = "fullstack-devops/awesome-ci"
     const downloadUrl = `https://github.com/${repo}/releases/download/${wantedVersion}/awesome-ci_${wantedVersion}_${wantedArch}`
-    const file = fs.createWriteStream(awesomeCiName);
-
+    // const file = fs.createWriteStream(awesomeCiName);
 
     // core.info(`Acquiring ${info.resolvedVersion} from ${info.downloadUrl}`);
     const downloadPath = await tc.downloadTool(downloadUrl, undefined);
+    core.info(`downloaded awesome-ci to ${downloadPath}`)
+    fs.rename(downloadPath+"/awesome-ci*", "awesome-ci", (err) => {
+        if (err) throw core.error(err);
+        core.info('successfully renamed awesome-ci!');
+    })
 
     const extPath = path.join(downloadPath, 'awesome-ci');
-
 
     core.info('Adding to the cache ...');
     const cachedDir = await tc.cacheDir(
