2e5cb180244848566fdc3fbd49952fd45a93d3f2
eksrvb <58111764+eksrvb@users.noreply.github.com>
2022-01-17 10:55:40 +0100

    add js for setup

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,37 @@
+import * as tc from '@actions/tool-cache';
 import * as core from '@actions/core';
+import * as path from 'path';
 import * as https from 'https';
 import * as fs from 'fs';
 
-function run() {
+async function run() {
     const wantedVersion = core.getInput('version');
     const wantedArch = core.getInput('arch');
-    
+
     const actionsPath = process.env.GITHUB_ACTION_PATH
-    const awesomeCiName = actionsPath+"/awesome-ci"
-    
+    const awesomeCiName = actionsPath + "/awesome-ci"
+
     // Download awesome-ci
     const repo = "fullstack-devops/awesome-ci"
     const downloadUrl = `https://github.com/${repo}/releases/download/${wantedVersion}/awesome-ci_${wantedVersion}_${wantedArch}`
     const file = fs.createWriteStream(awesomeCiName);
-    
-    https.get(downloadUrl, (res) => {
-        res.pipe(file)
-    }).on('error', (e) => {
-        core.setFailed("could not download awesome-ci: "+e)
-    });
-    
-    // add awesome-ci to path
-    core.addPath(awesomeCiName);
-    fs.chmod(awesomeCiName, 0o775, (err) => {
-        if (err) throw core.error(err);
-        core.info('The permissions for file "my_file.txt" have been changed!');
-    });
+
+
+    // core.info(`Acquiring ${info.resolvedVersion} from ${info.downloadUrl}`);
+    const downloadPath = await tc.downloadTool(downloadUrl, undefined);
+
+    const extPath = path.join(downloadPath, 'awesome-ci');
+
+
+    core.info('Adding to the cache ...');
+    const cachedDir = await tc.cacheDir(
+        extPath,
+        'awesome-ci',
+        wantedVersion
+    );
+    core.info(`Successfully cached awesome-ci to ${cachedDir}`);
 }
+
+
 
 run();
